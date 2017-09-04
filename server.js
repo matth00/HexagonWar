c3d56f3da334dc443732cf9372a204dbae780111
Unknown <lu.gindre@gmail.com>
2017-09-05 00:07:24 +0200

    added socket.io clients and server can now communicate

--- server.js
+++ server.js
@@ -1,37 +1,43 @@
 "use strict";
 
-const Log = require('log'),
-      log = new Log( "debug" ),
-      express = require("express"),
-      app = express(),
-      server = require("http").createServer(app),
-      fs = require('fs'),
-      io = require('socket.io')(server),
-      path = require('path');
+const port = 8080,
+    Log = require('log'),
+    log = new Log( "debug" ),
+    express = require("express"),
+    app = express(),
+    server = require("http").createServer(app),
+    fs = require('fs'),
+    io = require('socket.io')(server),
+    path = require('path');
 
 
 
 const gameEngine = require("./gameEngine.js");
 // Chargement de socket.io
 
-// Quand un client se connecte, on le note dans la console
-// io.sockets.on('connection', function (socket) {
-//     console.log('Un client est connecté !');
-// });
 
 app.use(express.static(path.join(__dirname, '/public')))
     .get('/', function(req, res){
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
-})
+        fs.readFile('./index.html', 'utf-8', function(error, content) {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(content);
+        });
+    })
     .get('/test', function(req, res){
-    res.writeHead(200, {"Content-Type":"text/plain"});
-    res.write("test");
-    res.end();
-})
+        res.writeHead(200, {"Content-Type":"text/plain"});
+        res.write("test");
+        res.end();
+    });
+
+io.on('connection', function (socket) {
+    log.info('new user connected');
+    socket.on('click', function (from, msg) {
+        log.info(from + ' clicked ' + msg);
+    });
+});
 
 
 
-server.listen(80);
+server.listen(port, function () {
+    log.info('listening on port ' + port);
+});
